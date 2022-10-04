03fcf747b699daa8cc5440b0824d34212c82c052
Alonso Montelongo <alonsomontelongo88@gmail.com>
2022-10-04 07:21:14 -0500

    These are my practice for math

---
+++ MathematicalComputingHomePractice/myfunction.py
@@ -0,0 +1,26 @@
+from tkinter import Y
+import numpy as np
+
+def f(x):
+    y = x**2
+    return y 
+def testf():
+    x= 3.0
+    y = f(x)
+    print(y)
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    testf()
+
+if __name__ == "__main__":
+    main()
